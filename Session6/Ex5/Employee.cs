ded2ce32102efd28903ec00698e80ddda80b9bfa
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-08-04 15:19:17 +0700

    o

---
+++ Session6/Ex5/Employee.cs
@@ -0,0 +1,27 @@
+﻿class Employee
+{
+    int _empId = 1;
+    string _empName = "James Anderson";
+    int _age = 25;
+    public void Display()
+    {
+        Console.WriteLine("Employee ID: " + _empId);
+        Console.WriteLine("Employee Name: " + _empName);
+    }
+}
+class Department: Employee
+{
+    int _deptId = 501;
+    string _depName = "Sales";
+    new void Display()
+    {
+        base.Display();
+        Console.WriteLine("Department ID: " + _deptId);
+        Console.WriteLine("Department Name: " + _depName);
+    }
+    static void Main(string[] args)
+    {
+        Department objDepartment = new Department();
+        objDepartment.Display();
+    }
+}
---
+++ Session6/Session6/Animal.cs
@@ -0,0 +1,28 @@
+﻿class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Everyanimal eats somethings. ");
+    }
+}
+class Mammal : Animal
+{
+    public void Feature()
+    {
+        Console.WriteLine("Mammals give birth to young ones");
+    }
+}
+class Dog : Mammal
+{
+    public void Noise()
+    {
+        Console.WriteLine("DogBarks.");
+    }
+    static void Main(string[] args)
+    {
+        Dog objDog = new Dog();
+        objDog.Eat();
+        objDog.Feature();
+        objDog.Noise();
+    }
+}
